da72cf815982c52525df0a98570fc08827d3e54b
choi sungwoog <baramman25@hanmail.net>
2018-12-30 10:51:55 +0900

    나의 첫번째 C# app 작성자_선경욱 2018.12.30

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System; // System 네이스페이스(안에 있는것들)을 쓰겠다
+namespace HelloWorld // 분류이름, 이름공간
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#~");
+        }
+    }
+}
